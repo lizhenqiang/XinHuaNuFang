76718def31abb00cd7eae06103e6b92b82a6b7de
Your Name <1294668228@qq.com>
2017-05-11 16:17:23 +0800

    ceshi

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,16 @@ package com.example.hasee.xinhuanufang;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv);
     }
 }
